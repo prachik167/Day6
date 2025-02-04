4850167b70c6784755a22765c70d627352216958
prachik167 <128143076+prachik167@users.noreply.github.com>
2025-02-04 13:50:53 +0530

    Add files via upload

---
+++ Day6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day5
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Day6/RoomNonAC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    internal class RoomNonAC
+    {
+        public int RoomNo { get; set; }
+        public string RoomType { get; set; }
+        public int Price { get; set; }
+    }
+}
